57ccce3e1ad8286aa961536c8ffa79b84cb9ec21
Michael Shick <m@shick.us>
2026-05-19 11:48:35 -0400

    updates

--- vitest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom/vitest';
-import { configure } from '@testing-library/dom';
-
-// Make getByText / getAllByText respect aria-hidden so that Radix UI's open
-// dialog (which hides the rest of the page with aria-hidden to trap focus)
-// doesn't cause false "multiple elements found" errors.
-configure({ defaultIgnore: 'script, style, [aria-hidden="true"] *' });
